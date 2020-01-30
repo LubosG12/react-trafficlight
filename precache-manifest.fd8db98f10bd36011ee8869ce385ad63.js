b62b40ebebef1662ce50b457c3614bf988d6fa48
Lubos Jones <l.furinda@gmail.com>
2020-01-30 21:04:40 +0100

    Updates

---
+++ precache-manifest.fd8db98f10bd36011ee8869ce385ad63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e2cdd4b34068fbe6498cd234041569",
+    "url": "/react-trafficlight/index.html"
+  },
+  {
+    "revision": "9792f0fbefbec32b33a0",
+    "url": "/react-trafficlight/static/css/main.d0b94b8f.chunk.css"
+  },
+  {
+    "revision": "f36520a0a67aacf53a66",
+    "url": "/react-trafficlight/static/js/2.0d6a0ae7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-trafficlight/static/js/2.0d6a0ae7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9792f0fbefbec32b33a0",
+    "url": "/react-trafficlight/static/js/main.61b52fad.chunk.js"
+  },
+  {
+    "revision": "2e3a5fb3aaa7c8ca2b97",
+    "url": "/react-trafficlight/static/js/runtime-main.5b638e0f.js"
+  }
+]);
